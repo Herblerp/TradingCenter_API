bfaacba0b34bfba18d3ef3dfe5f273b65da4988a
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2018-11-07 18:34:31 +0100

    Add generic repository

---
+++ Tradingcenter.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trainingcenter.Domain.Repositories;
+
+namespace Tradingcenter.Data.Repositories
+{
+    class GenericRepository : IGenericRepository
+    {
+        public Task AddAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Trainingcenter.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trainingcenter.Domain.Entities;
+
+namespace Trainingcenter.Domain.Repositories
+{
+    public interface IGenericRepository
+    {
+        Task AddAsync<T>(T entity) where T: class;
+        Task DeleteAsync<T>(T entity) where T : class;
+    }
+}
