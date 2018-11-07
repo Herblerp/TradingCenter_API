f3456e7f9281f144ee15cbf80822ecc6b52c92f0
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2018-11-07 15:46:44 +0100

    Update user model

--- Trainingcenter.DL/Entities/User.cs
+++ Trainingcenter.DL/Entities/User.cs
@@ -10,16 +10,15 @@ namespace Trainingcenter.DL.Entities
         public int PictureId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Company { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
-        public string PostalCode { get; set; }
         public string Phone { get; set; }
-        public string Fax { get; set; }
         public string Email { get; set; }
+
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PassWordSalt { get; set; }
+
+        public List<Order> Orders { get; set; }
     }
 }
