a83e7d433abf5e9cee2d51283ed4a307f96d217e
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2018-11-07 15:42:19 +0100

    Add user entity

---
+++ Trainingcenter.DL/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainingcenter.DL.Entities
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public int PictureId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+    }
+}
