42078149b70cb74bbc95da65a434c17377a4d4a4
Herblerp <Quentin.vekeman@gmail.com>
2018-11-08 12:17:24 +0100

    add Binance API

---
+++ BinanceAPI/AllTradesInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Binance_API
+{
+    public class AllTradesInfo
+    {
+        [JsonProperty("symbol")]
+        public static string Symbol { get; set; }
+
+       //[JsonProperty("orderId")]
+        //public long OrderId { get; set; }
+
+        //[JsonProperty("clientOrderId")]
+        //public string ClientOrderId { get; set; }
+
+        //[JsonProperty("price")]
+        //public decimal Price { get; set; }
+
+        [JsonProperty("origQty")]
+        public static decimal OriginalQuantity { get; set; }
+
+        [JsonProperty("executedQty")]
+        public static decimal ExecutedQuantity { get; set; }
+
+        //[JsonProperty("status")]
+        //public OrderStatus Status { get; set; }
+
+        //[JsonProperty("timeInForce")]
+        //public TimeInForce TimeInForce { get; set; }
+
+        //[JsonProperty("type")]
+        //public OrderType OrderType { get; set; }
+
+        [JsonProperty("side")]
+        public static OrderSide OrderSide { get; set; }
+
+        //[JsonProperty("stopPrice")]
+        //public decimal StopPrice { get; set; }
+
+        //[JsonProperty("icebergQty")]
+        //public decimal IcebergQuantity { get; set; }
+
+        [JsonProperty("time")]
+        public static long Time { get; set; }
+    }
+}
---
+++ BinanceAPI/OrderType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binance_API
+{
+    public class OrderType
+    {
+        private string Value { get; }
+        private OrderType(string value) { Value = value; }
+
+        public static OrderType Limit => new OrderType("LIMIT");
+        public static OrderType Market => new OrderType("MARKET");
+
+        public static implicit operator string(OrderType orderType) => orderType.Value;
+        public static implicit operator OrderType(string text) => new OrderType(text);
+        public override string ToString() => Value;
+    }
+}
