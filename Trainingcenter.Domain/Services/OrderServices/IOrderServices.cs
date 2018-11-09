2824b930a2f1fa1985599e7b661803a4e472c873
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2018-11-09 19:47:38 +0100

    add order service classes

---
+++ Trainingcenter.Domain/Services/OrderServices/IOrderServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trainingcenter.Domain.DomainModels;
+
+namespace Trainingcenter.Domain.Services.OrderServices
+{
+    interface IOrderServices
+    {
+        Task<List<Order>> GetOrdersFromUserId(int userId, DateTime dateFrom, DateTime dateTo);
+        Task<List<Order>> ResfreshOrdersFromUserId(int userId);
+    }
+}
---
+++ Trainingcenter.Domain/Services/OrderServices/OrderServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainingcenter.Domain.Repositories;
+
+namespace Trainingcenter.Domain.Services.OrderServices
+{
+    class OrderServices
+    {
+        private readonly IOrderRepository _orderRepo;
+
+        public OrderServices(IOrderRepository orderRepo)
+        {
+            _orderRepo = orderRepo;
+        }
+
+    }
+}
